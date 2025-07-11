6df0bd099415b6baa3bbce718085b6b90e121f74
Acha <62490806+Achalogy@users.noreply.github.com>
2025-07-11 13:54:31 -0500

    Update Hash.h

--- Strings/Hash.h
+++ Strings/Hash.h
@@ -5,28 +5,34 @@ For build $O(|s|)$ and $O(1)$ per query.
 
 class HashedString {
   private:
-	static const ll M = 1e9 + 9;
-	static const ll B = 9973;
+	// change M and B if you want
+	static const ll M = (1LL << 61) - 1;
+	static const ll B;
  
 	// pow[i] contains B^i % M
-	static vl pow;
+	static vector<ll> pow;
  
 	// p_hash[i] is the hash of the first i characters of the given string
-	vl p_hash;
+	vector<ll> p_hash;
+ 
+	_int128 mul(ll a, ll b) { return (_int128)a * b; }
+	ll mod_mul(ll a, ll b) { return mul(a, b) % M; }
  
   public:
-	HashedString(const string &s) : p_hash(len(s) + 1) {
-		while (len(pow) < len(s)) { pow.pb((pow.back() * B) % M); } 
+	HashedString(const string &s) : p_hash(s.size() + 1) {
+		while (pow.size() < s.size()) { pow.push_back(mod_mul(pow.back(), B)); }
 		p_hash[0] = 0;
-		forn(i, len(s)) {
-			p_hash[i + 1] = ((p_hash[i] * B) % M + s[i]) % M;
+		for (int i = 0; i < s.size(); i++) {
+			p_hash[i + 1] = (mul(p_hash[i], B) + s[i]) % M;
 		}
 	}
  
 	ll get_hash(int start, int end) {
-		long long raw_val =
-		    (p_hash[end + 1] - (p_hash[start] * pow[end - start + 1]));
-		return (raw_val % M + M) % M;
+		ll raw_val = p_hash[end + 1] - mod_mul(p_hash[start], pow[end - start + 1]);
+		return (raw_val + M) % M;
 	}
 };
-vl HashedString::pow = {1};
+ 
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+vector<ll> HashedString::pow = {1};
+const ll HashedString::B = uniform_int_distribution<ll>(0, M - 1)(rng);
